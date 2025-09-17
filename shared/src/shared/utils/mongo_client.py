941c4a60e9dbeb98275935ec02370df810bdb16b
Amar Yadav <ayadav42@asu.edu>
2025-09-17 16:43:01 -0700

    replaced motor with pymongo

--- shared/src/shared/utils/mongo_client.py
+++ shared/src/shared/utils/mongo_client.py
@@ -1,6 +1,6 @@
 import os
-from beanie import init_beanie  # type: ignore
-from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from pymongo import AsyncMongoClient  # <-- switched from Motor
 from pydantic_settings import BaseSettings
 
 from shared.models.db.manufacturer import Manufacturer
@@ -11,25 +11,21 @@ from shared.models.db.user import User
 from data_etl_app.models.keyword_ground_truth import KeywordGroundTruth
 from data_etl_app.models.binary_ground_truth import BinaryGroundTruth
 
+
 MONGO_DB_URI = os.getenv("MONGO_DB_URI")
 if not MONGO_DB_URI:
     raise ValueError("MONGO_DB_URI environment variable is not set.")
 
 
-# 1. Define Pydantic Settings for DB
 class Settings(BaseSettings):
-    MONGO_URI: str = (
-        # "mongodb://root:example@localhost:27017/sudokn?authSource=admin&directConnection=true"
-        # "mongodb://root:M!o2N%23g4O%25@18.207.184.14:27017/sudokn?authSource=admin&directConnection=true"
-        MONGO_DB_URI
-    )
+    MONGO_URI: str = MONGO_DB_URI
 
 
 settings = Settings()
 
 
 async def init_db():
-    client = AsyncIOMotorClient(settings.MONGO_URI)  # type: ignore
+    client = AsyncMongoClient(settings.MONGO_URI)  # <-- switched here
     return await init_beanie(
         database=client.get_default_database(),
         document_models=[
